071528105a16e7106b0b204a58e916fe661ac8d5
Dusan Lukic <dusanlukic404@gmail.com>
2022-08-15 19:37:45 +0200

    added skeleton animation, fetch data and display cards properly

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Filter region
+// Filter button
 
 const filterBtn = document.querySelector(".filter");
 const filterList = document.querySelector(".filter-list");
@@ -8,3 +8,55 @@ const filterList = document.querySelector(".filter-list");
 filterBtn.addEventListener("click", function () {
   filterList.classList.toggle("filter-list--active");
 });
+
+// Getting data from API
+
+const getData = async function (url) {
+  const response = await fetch(url);
+  const data = response.json();
+
+  return data;
+};
+
+// Skeleton animation on loading data
+
+const cardTemplate = document.getElementById("card-template");
+const cardContainer = document.querySelector(".card-list");
+
+for (let i = 0; i < 8; i++) {
+  cardContainer.append(
+    document.querySelector("#skeleton-template").content.cloneNode(true)
+  );
+}
+
+// Displaying cards
+
+getData("https://restcountries.com/v3.1/all").then((countries) => {
+  cardContainer.innerHTML = "";
+  countries.forEach((country) => {
+    const card = cardTemplate.content.cloneNode(true);
+    card.querySelector(" img").src = country.flags.svg;
+    card
+      .querySelector("img")
+      .setAttribute("alt", `Flag of ${country.name.common}`);
+    card.querySelector("h2").textContent = country.name.common;
+    card.querySelector(`[data-value='population']`).textContent =
+      formatPopulation(country.population);
+    card.querySelector(`[data-value='region']`).textContent = country.region;
+    card.querySelector(`[data-value="capital"]`).textContent = country.capital;
+
+    cardContainer.append(card);
+  });
+
+  gsap.from(".card", {
+    opacity: 0,
+    y: 20,
+    duration: 1,
+    stagger: 0.2,
+    ease: "back.easeOut",
+  });
+});
+
+function formatPopulation(num) {
+  return new Intl.NumberFormat().format(num);
+}
